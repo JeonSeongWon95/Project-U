8ee381b27697a1696d2c9ecf2efcd01a26baafeb
Jeon SeongWon <fpdlsnsqlc3@naver.com>
2026-01-08 00:08:01 +0900

    1. Move Action을 통해 들어온 데이터를 바탕으로 캐릭터 회전 구현

--- Assets/Scripts/Character/States/PlayerMoveState.cs
+++ Assets/Scripts/Character/States/PlayerMoveState.cs
@@ -3,6 +3,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerMoveState : PlayerBaseState
 {
+    public enum EDIRECTION
+    {
+        North = 0,
+        South = 180,
+        East = 90,
+        West = 270,
+        North_West = 315,
+        South_East = 135,
+        North_East = 45,
+        South_West = 225
+    }
     public PlayerMoveState(PlayerStateMachine newPlayer) : base(newPlayer) {}
 
     public override void Enter()
@@ -22,7 +33,27 @@ public class PlayerMoveState : PlayerBaseState
 
         Vector3 moveAmount = new Vector3(player.moveAmount.x, 0, player.moveAmount.y);
         moveAmount.Normalize();
-        player.transform.Translate(moveAmount * 5.0f * Time.deltaTime);
+
+
+        EDIRECTION direction = EDIRECTION.North;
+
+        if (player.moveAmount.y == 0)
+        {
+            direction = player.moveAmount.x > 0 ? EDIRECTION.East : EDIRECTION.West;
+        }
+        else if (player.moveAmount.y < 0) 
+        {
+            direction = player.moveAmount.x == 0 ? EDIRECTION.South : player.moveAmount.x > 0 ? EDIRECTION.South_East : EDIRECTION.South_West;
+        }
+        else
+        {
+            direction = player.moveAmount.x == 0 ? EDIRECTION.North : player.moveAmount.x > 0 ? EDIRECTION.North_East : EDIRECTION.North_West;
+        }
+
+        Vector3 angle = player.transform.eulerAngles; //(player.transform.up, (float)direction);
+        angle.y = (float)direction;
+        player.transform.eulerAngles = angle;
+        player.transform.Translate(moveAmount * 5.0f * Time.deltaTime, Space.World);
 
     }
 
